228c9655a4ce9fded90ea992cf46e8107630d431
Franco Natanael Tomaino <tomainofranco5@gmail.com>
2025-07-18 19:08:10 -0300

    Create next.js app and delete default code

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className='text-black text-center p-5'>Rick and Morty App</p>
+    </div>
+  );
+}
